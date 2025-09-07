378e479b583d6c6911db2b9ecaae86ab728bd0df
Anwesha Bhadury <bhaduryanwesha07@gmail.com>
2025-09-07 12:18:29 +0530

    Saved Changes

--- src/sanity.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@sanity/client';
-
-export const client = createClient({
-  projectId: 'kn8ctvrb',
-  dataset: 'production',
-  useCdn: false,          // 🔴 set to false for fresh data
-  apiVersion: '2025-09-06',
-});
