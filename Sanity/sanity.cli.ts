ee66f6b105cd94882074559c1f44261d2549dd3d
Mainak Deb <debmainak37@gmail.com>
2025-09-15 14:20:04 +0530

    latest change

--- Sanity/sanity.cli.ts
+++ Sanity/sanity.cli.ts
@@ -5,6 +5,5 @@ export default defineCliConfig({
     projectId:process.env.SANITY_STUDIO_PROJECT_ID,
     dataset:process.env.SANITY_STUDIO_DATASET
   },
-  studioHost: 'entranc',  // <= this line stops the prompt
   autoUpdates: true,         // CLI will keep your studio dependencies updated
 })
